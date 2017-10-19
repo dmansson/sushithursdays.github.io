28ceeda50b73597d9405ec08abfe8b5424e40a70
dmansson <32701558+dmansson@users.noreply.github.com>
2017-10-19 13:28:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+	var d = new Date();
+	var n = d.getDay();
+
+	if (n === 4) {
+	window.location.href = "torsdag.html";
+
+}
+else {
+	window.location.href = "index1.html";
+}
+}
+
+
+
+
